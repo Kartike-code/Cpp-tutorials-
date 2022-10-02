780c7e67a75078c860096221e9cb67b7863663d4
Kartike-code <110462344+Kartike-code@users.noreply.github.com>
2022-10-02 10:24:13 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,21 @@
+// *********************BASIC STRUCTURE OF A C++ PROGRAM****************************
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
+
+/*1. In the above code "#include<iostream>"" is the name of a header file wherein iostream is a library
+which helps us to get input data and show output data. And because of this file we can use 
+functions like cout, cin, etc.
+2. "using namespace std" means that we are using the standard namespace library of the iostream library
+to execute cout function.
+3. "int" is a return type which means an "integer" and "main()" is a function that is a primary function
+which helps to call other functions like "cout" and "cin", etc. "()" denotes that it is function.
+4. Anything which is written in double quotations represents that it is a string.
+5. The importance of "return 0" is that the keyword "return" will return a 0 value as an integer to the
+main function.*/
