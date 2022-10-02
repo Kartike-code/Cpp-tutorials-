d22e5dd31656dffd45a80bf92e42bfeea8de344d
Kartike-code <110462344+Kartike-code@users.noreply.github.com>
2022-10-02 10:24:36 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,17 @@
+// ************************VARIABLES AND COMMENTS***********************
+
+// 1. Comments:- Comments are human readable text in the source codes which are ignored
+// by the compiler and is not printed or included in the output.
+// 2. Variables:- Variables are the containers which are used to store our data.
+// There are many data types like "int" which includes integer type numbers
+// and "float" which includes floating point no.s ,that is, low precision decimal numbers
+// and also "char" which includes characters like a,b,c,d,f,g,h,j,etc.
+
+# include<iostream>
+using namespace std;
+//This file was created by Kartike Goel
+int main(){
+    int sum = 6;
+    cout<<"Hello World"<< sum;
+    return 0;
+}
