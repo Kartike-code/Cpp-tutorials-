29aa5741d1dd8301c515e53f8b067186e3aaa117
Kartike-code <110462344+Kartike-code@users.noreply.github.com>
2022-10-02 10:23:56 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,17 @@
+// *************************INTRODUCTION TO PROGRAMMING AND C++**************************
+
+/*1.Programming:- Programming can be understood as a way to commnicate with the computer(machines) 
+and to give instructions to it.
+2.Programming language:- Programming language helps us to communicate with the computer just like
+we communicate with help of languages like English, Hindi, Tamil, etc.
+3. C++ was an added of the C language developed by Bjarne Stroustrup in the 1980s.
+it is very close to the hardware and due to its object orientedness it is one of the most 
+preferred languages.*/
+// Here we will write a basic C++ program:-
+
+#include<iostream>
+
+int main(){ 
+    std::cout<<"Hello World";    
+    return 0;
+}
